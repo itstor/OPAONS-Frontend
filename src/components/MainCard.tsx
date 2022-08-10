c6c0e5a4fd1085f4c08f8d44e65b4ed62b419830
itstor <ahmdthoriq5@gmail.com>
2022-08-10 14:17:54 +0700

    feat: maincard and subcard

---
+++ src/components/MainCard.tsx
@@ -0,0 +1,71 @@
+import { Card, CardContent, CardHeader, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { ColorPartial } from '@mui/material/styles/createPalette';
+import { forwardRef } from 'react';
+
+const headerSX = {
+  '& .MuiCardHeader-action': { mr: 0 },
+};
+
+type MainCardProps = {
+  border?: boolean;
+  boxShadow?: boolean;
+  children?: React.ReactNode;
+  content?: boolean;
+  contentClass?: string;
+  contentSX?: object;
+  darkTitle?: boolean;
+  secondary?: React.ReactNode;
+  shadow?: string;
+  sx?: object;
+  title?: React.ReactNode;
+};
+
+const MainCard = forwardRef(
+  (
+    {
+      border = true,
+      boxShadow,
+      children,
+      content = true,
+      contentClass = '',
+      contentSX = {},
+      darkTitle,
+      secondary,
+      shadow,
+      sx = {},
+      title,
+      ...others
+    }: MainCardProps,
+    ref: React.Ref<HTMLDivElement>
+  ) => {
+    const theme = useTheme();
+
+    return (
+      <Card
+        ref={ref}
+        {...others}
+        sx={{
+          border: border ? '1px solid' : 'none',
+          borderColor: (theme.palette.primary as ColorPartial)[200] ?? 0 + 75,
+          ':hover': {
+            boxShadow: boxShadow ? shadow || '0 2px 14px 0 rgb(32 40 45 / 8%)' : 'inherit',
+          },
+          ...sx,
+        }}
+      >
+        {!darkTitle && title && <CardHeader sx={headerSX} title={title} action={secondary} />}
+        {darkTitle && title && <CardHeader sx={headerSX} title={<Typography variant='h3'>{title}</Typography>} action={secondary} />}
+
+        {content && (
+          <CardContent sx={contentSX} className={contentClass}>
+            {children}
+          </CardContent>
+        )}
+        {!content && children}
+      </Card>
+    );
+  }
+);
+
+export default MainCard;
---
+++ src/components/SubCard.tsx
@@ -0,0 +1,50 @@
+import { Card, CardContent, CardHeader, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { forwardRef } from 'react';
+
+type SubCardProps = {
+  children?: React.ReactNode;
+  content?: boolean;
+  contentClass?: string;
+  darkTitle?: boolean;
+  secondary?: React.ReactNode;
+  sx?: object;
+  contentSX?: object;
+  title?: string;
+};
+
+const SubCard = forwardRef(
+  (
+    { children, content = true, contentClass, darkTitle, secondary, sx = {}, contentSX = {}, title, ...others }: SubCardProps,
+    ref: React.Ref<HTMLDivElement>
+  ) => {
+    const theme = useTheme();
+
+    return (
+      <Card
+        ref={ref}
+        sx={{
+          border: '1px solid',
+          borderColor: theme.palette.primary.light,
+          ':hover': {
+            boxShadow: '0 2px 14px 0 rgb(143 93 34 / 8%)',
+          },
+          ...sx,
+        }}
+        {...others}
+      >
+        {!darkTitle && title && <CardHeader sx={{ p: 2.5 }} title={<Typography variant='h5'>{title}</Typography>} action={secondary} />}
+        {darkTitle && title && <CardHeader sx={{ p: 2.5 }} title={<Typography variant='h4'>{title}</Typography>} action={secondary} />}
+
+        {content && (
+          <CardContent sx={{ p: 2.5, ...contentSX }} className={contentClass || ''}>
+            {children}
+          </CardContent>
+        )}
+        {!content && children}
+      </Card>
+    );
+  }
+);
+
+export default SubCard;
