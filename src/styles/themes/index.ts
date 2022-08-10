98c08ca4145f4a3964f4de5b973c1da8290a0c15
itstor <ahmdthoriq5@gmail.com>
2022-08-10 09:20:00 +0700

    feat: app theme

---
+++ src/styles/themes/index.ts
@@ -0,0 +1,65 @@
+import { createTheme, Theme, ThemeOptions } from '@mui/material/styles';
+
+import colors from '@/styles/colors/theme-colors.module.scss';
+
+import componentStyleOverrides from './compStyleOverride';
+import themePalette from './palette';
+import themeTypography from './typography';
+
+export type IThemeOption = {
+  colors: { readonly [key: string]: string };
+  heading: string;
+  paper: string;
+  backgroundDefault: string;
+  background: string;
+  darkTextPrimary: string;
+  darkTextSecondary: string;
+  textDark: string;
+  menuSelected: string;
+  menuSelectedBack: string;
+  divider: string;
+  fontFamily: string;
+  borderRadius: number;
+};
+
+export const theme = (): Theme => {
+  const color = colors;
+
+  const themeOption: IThemeOption = {
+    colors: color,
+    heading: color.grey900,
+    paper: color.paper,
+    backgroundDefault: color.paper,
+    background: color.primaryLight,
+    darkTextPrimary: color.grey700,
+    darkTextSecondary: color.grey500,
+    textDark: color.grey900,
+    menuSelected: color.secondaryDark,
+    menuSelectedBack: color.secondaryLight,
+    divider: color.grey200,
+    fontFamily: 'Poppins, sans-serif',
+    borderRadius: 8,
+  };
+
+  const themeOptions: ThemeOptions = {
+    direction: 'ltr',
+    palette: themePalette(themeOption),
+    mixins: {
+      toolbar: {
+        minHeight: '48px',
+        padding: '16px',
+        '@media (min-width: 600px)': {
+          minHeight: '48px',
+        },
+      },
+    },
+    typography: themeTypography(themeOption),
+  };
+
+  const themes: Theme = createTheme(themeOptions);
+  themes.components = componentStyleOverrides(themeOption);
+
+  return themes;
+};
+
+export default theme;
---
+++ src/styles/themes/palette.ts
@@ -0,0 +1,77 @@
+import { PaletteOptions } from '@mui/material';
+
+import { IThemeOption } from '@/styles/themes';
+
+declare module '@mui/material/styles/createPalette' {
+  interface Palette {
+    orange?: Palette['primary'];
+  }
+  interface PaletteOptions {
+    orange?: PaletteOptions['primary'];
+  }
+  interface TypeText {
+    dark?: string | undefined;
+    hint?: string | undefined;
+  }
+}
+
+export default function themePalette(theme: IThemeOption): PaletteOptions {
+  return {
+    common: {
+      black: theme.colors?.darkPaper,
+    },
+    primary: {
+      light: theme.colors?.primaryLight,
+      main: theme.colors?.primaryMain,
+      dark: theme.colors?.primaryDark,
+      200: theme.colors?.primary200,
+      800: theme.colors?.primary800,
+    },
+    secondary: {
+      light: theme.colors?.secondaryLight,
+      main: theme.colors?.secondaryMain,
+      dark: theme.colors?.secondaryDark,
+      200: theme.colors?.secondary200,
+      800: theme.colors?.secondary800,
+    },
+    error: {
+      light: theme.colors?.errorLight,
+      main: theme.colors?.errorMain,
+      dark: theme.colors?.errorDark,
+    },
+    warning: {
+      light: theme.colors?.warningLight,
+      main: theme.colors?.warningMain,
+      dark: theme.colors?.warningDark,
+    },
+    success: {
+      light: theme.colors?.successLight,
+      200: theme.colors?.success200,
+      main: theme.colors?.successMain,
+      dark: theme.colors?.successDark,
+    },
+    grey: {
+      50: theme.colors?.grey50,
+      100: theme.colors?.grey100,
+      500: theme.darkTextSecondary,
+      600: theme.heading,
+      700: theme.darkTextPrimary,
+      900: theme.textDark,
+    },
+    text: {
+      primary: theme.darkTextPrimary,
+      secondary: theme.darkTextSecondary,
+      dark: theme.textDark,
+      hint: theme.colors?.grey100,
+    },
+    background: {
+      paper: theme.paper,
+      default: theme.backgroundDefault,
+    },
+    orange: {
+      light: theme.colors?.orangeLight,
+      main: theme.colors?.orangeMain,
+      dark: theme.colors?.orangeDark,
+    },
+  };
+}
