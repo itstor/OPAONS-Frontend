71eb8e4a965e56e8b547a6c3d6f015146647164b
itstor <ahmdthoriq5@gmail.com>
2022-08-11 10:26:55 +0700

    feat: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,25 +1,22 @@
+import { Button } from '@mui/material';
+import Lottie from 'lottie-react';
 import * as React from 'react';
-import { RiAlarmWarningFill } from 'react-icons/ri';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
 import Seo from '@/components/Seo';
+
+import Anim404 from '~/anim/404.json';
 
 export default function NotFoundPage() {
   return (
     <Layout>
-      <Seo templateTitle='Not Found' />
-
+      <Seo templateTitle='404' />
       <main>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-            <RiAlarmWarningFill size={60} className='drop-shadow-glow animate-flicker text-red-500' />
-            <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
-            <ArrowLink className='mt-4 md:text-lg' href='/'>
-              Back to Home
-            </ArrowLink>
-          </div>
-        </section>
+        <div className='layout flex min-h-screen flex-col items-center justify-center bg-corner-login bg-left-bottom bg-no-repeat text-center text-black'>
+          <Lottie className='w-full max-w-xs md:max-w-md 2xl:max-w-lg' animationData={Anim404} />
+          <h1 className='mt-5 text-3xl md:text-4xl 2xl:text-5xl'>Page Not Found</h1>
+          <Button size='large'>Back to home</Button>
+        </div>
       </main>
     </Layout>
   );
