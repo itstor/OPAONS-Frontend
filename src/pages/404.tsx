afadcf4677d9ea07318c7ad65100e2927f22d23d
itstor <ahmdthoriq5@gmail.com>
2022-08-11 10:28:43 +0700

    fix: back button

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@mui/material';
 import Lottie from 'lottie-react';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
@@ -8,14 +9,18 @@ import Seo from '@/components/Seo';
 import Anim404 from '~/anim/404.json';
 
 export default function NotFoundPage() {
+  const router = useRouter();
+
   return (
     <Layout>
       <Seo templateTitle='404' />
       <main>
-        <div className='layout flex min-h-screen flex-col items-center justify-center bg-corner-login bg-left-bottom bg-no-repeat text-center text-black'>
+        <div className='flex min-h-screen flex-col items-center justify-center bg-corner-login bg-left-bottom bg-no-repeat text-center text-black'>
           <Lottie className='w-full max-w-xs md:max-w-md 2xl:max-w-lg' animationData={Anim404} />
           <h1 className='mt-5 text-3xl md:text-4xl 2xl:text-5xl'>Page Not Found</h1>
-          <Button size='large'>Back to home</Button>
+          <Button size='large' onClick={() => router.push('/')}>
+            Back to home
+          </Button>
         </div>
       </main>
     </Layout>
