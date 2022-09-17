51e059a61dc7f2b0d2f69394d0e7b7518a61213a
itstor <ahmdthoriq5@gmail.com>
2022-09-17 21:53:17 +0700

    feat: update peraturan

--- src/data/peraturan-data.ts
+++ src/data/peraturan-data.ts
@@ -1,11 +1,16 @@
 export const PeraturanOPA = [
-  'Peserta OPA UNS 2022 stand by pada pukul 07.30 WIB dalam pelaksanaan pengerjaan soal babak penyisihan OPA UNS 2022 dan sudah melakukan langkah-langkah yang akan kami sebutkan di bawah ini',
-  'Setiap anggota tim menyiapkan satu komputer atau laptop (PC) yang terkoneksi dengan jaringan internet, sehingga dalam satu tim terdapat 3 komputer atau laptop (PC). Pengerjaan soal bersifat individu, sehingga setiap anggota tim akan mendapat username, password yang berbeda untuk mengerjakan soal secara online',
-  'Username dan password akan diberikan kepada peserta OPA UNS 2022 pada Jumat, 10 September 2022 ',
-  'Pada pukul 07.45 WIB silakan peserta OPA UNS 2022 mulai mengerjakan soal babak penyisihan dengan membuka website OPAUNS yang telah diinstal sebelumnya',
-  'Log in dengan menggunakan username dan password yang telah diberikan panitia',
-  'Terdapat 75 soal pilihan ganda yang dikerjakan selama 105 menit tanpa adanya  batasan waktu untuk setiap soal',
-  'Pengerjaan soal dapat dari nomor berapa saja dan apabila terdapat waktu sisa diperkenankan untuk meneliti kembali',
-  'Apabila sudah selesai silakan klik pada tombol selesai dan hasil pengerjaan akan diterima oleh panitia',
-  'Setiap jawaban benar untuk soal <strong>mudah</strong> bernilai <strong>1 poin</strong>, untuk soal <strong>sedang</strong> bernilai <strong>2 poin</strong>, untuk soal <strong>sulit</strong> bernilai <strong>3 poin</strong>, dan untuk soal <strong>HOTS</strong> bernilai <strong>4 poin</strong>, <strong>salah -1</strong> dan <strong>tidak menjawab 0</strong>. Soal babak penyisihan OPA UNS 2022 yang dikerjakan setiap individu akan kami akumulasikan.',
+  `Peserta OPA UNS 2022 stand by pada pukul 07.30 WIB dalam pelaksanaan pengerjaan soal babak penyisihan OPA UNS 2022 dan sudah melakukan langkah-langkah yang akan kami sebutkan di bawah ini`,
+  `Setiap anggota tim menyiapkan satu komputer atau laptop (PC) yang terkoneksi dengan jaringan internet, sehingga dalam satu tim terdapat 3 komputer atau laptop (PC). Pengerjaan soal bersifat individu, sehingga setiap anggota tim akan mendapat username, password yang berbeda untuk mengerjakan soal secara online`,
+  `Username dan password akan diberikan kepada peserta OPA UNS 2022 pada <strong>Minggu, 18 September 2022</strong>`,
+  `Pada pukul 07.45 WIB silakan peserta OPA UNS 2022 mulai mengerjakan soal babak penyisihan dengan membuka website OPAUNS yang telah diinstal sebelumnya`,
+  `Log in dengan menggunakan username dan password yang telah diberikan panitia`,
+  `Peserta akan dihadapkan dengan <strong>25 soal pilihan ganda, 10 soal isian singkat</strong> dan <strong>5 soal essay</strong> untuk SMA maupun SMK dengan alokasi waktu <strong>110 menit</strong>`,
+  `Pengerjaan soal dapat dari nomor berapa saja dan apabila terdapat waktu sisa diperkenankan untuk meneliti kembali`,
+  `Apabila sudah selesai silakan klik pada tombol selesai dan hasil pengerjaan akan diterima oleh panitia`,
+  `Sistem penilaian untuk babak ini yaitu
+  <ul>
+  <li><strong>Pilihan Ganda</strong> (jawaban <strong>Benar</strong> bernilai <strong>4 poin</strong>, jawaban <strong>Salah</strong> bernilai <strong>-1 poin</strong> dan apabila tidak dijawab <strong>0 poin</strong>.)</li>
+  <li><strong>Isian Singkat</strong> (jawaban <strong>Benar</strong> bernilai <strong>5 poin</strong>, jawaban <strong>Salah</strong> bernilai <strong>-1 poin</strong> dan apabila tidak dijawab <strong>0 poin</strong>.)</li>
+  <li><strong>Essay</strong> (jawaban <strong>Benar</strong> bernilai <strong>10 poin</strong>, jawaban <strong>Salah</strong> bernilai <strong>1 poin</strong> dan apabila tidak dijawab <strong>0 poin</strong>.)</li>
+  </ul>`,
 ];
